0f2a2b3a924244009816b05b6ff3cb31dcc3b19b
Manuel <NietoSoftwareDeveloper@outlook.com>
2026-08-05 23:04:37 -0500

    Update domtree.js

--- js/domtree.js
+++ js/domtree.js
@@ -1,100 +1,100 @@
 const width = 330;
 const height = 600;
 const quantity = 180;
-const types = [ 'text', 'select', 'progress', 'meter', 'button', 'radio', 'checkbox' ];
-const greetings = [ 'HTML','JS','CSS','My SQl','React','Node JS','Astro','Docker','Java' ];
+const types = ['text', 'select', 'progress', 'meter', 'button', 'radio', 'checkbox'];
+const greetings = ['HTML', 'JS', 'CSS', 'My SQl', 'React', 'Node JS', 'Astro', 'Docker', 'Java'];
 
-let tree = document.querySelector( '.tree' ),
+let tree = document.querySelector('.tree'),
 	treeRotation = 0;
 
 tree.style.width = width + 'px';
 tree.style.height = height + 'px';
 
-window.addEventListener( 'resize', resize, false );
+window.addEventListener('resize', resize, false);
 
-for( var i = 0; i < quantity; i++ ) {
+for (var i = 0; i < quantity; i++) {
 	let element = null,
-		type = types[ Math.floor( Math.random() * types.length ) ],
-		greeting = greetings[ Math.floor( Math.random() * greetings.length ) ];
+		type = types[Math.floor(Math.random() * types.length)],
+		greeting = greetings[Math.floor(Math.random() * greetings.length)];
 
-	let x = width/2,
-		y = Math.round( Math.random() * height );
+	let x = width / 2,
+		y = Math.round(Math.random() * height);
 
 	let rx = 0,
 		ry = Math.random() * 360,
 		rz = -Math.random() * 18;
 
-	let elemenWidth = 5 + ( ( y / height ) * width / 2 ),
+	let elemenWidth = 5 + ((y / height) * width / 2),
 		elemenHeight = 26;
 
-	switch( type ) {
+	switch (type) {
 		case 'button':
-			element = document.createElement( 'button' );
+			element = document.createElement('button');
 			element.textContent = greeting;
 			element.style.width = elemenWidth + 'px';
 			element.style.height = elemenHeight + 'px';
 			break;
 		case 'progress':
-			element = document.createElement( 'progress' );
+			element = document.createElement('progress');
 			element.style.width = elemenWidth + 'px';
 			element.style.height = elemenHeight + 'px';
-			if( Math.random() > 0.5 ) {
-				element.setAttribute( 'max', '100' );
-				element.setAttribute( 'value', Math.round( Math.random() * 100 ) );
+			if (Math.random() > 0.5) {
+				element.setAttribute('max', '100');
+				element.setAttribute('value', Math.round(Math.random() * 100));
 			}
 			break;
 		case 'select':
-			element = document.createElement( 'select' );
-			element.setAttribute( 'selected', greeting );
-			element.innerHTML = '<option>' + greetings.join( '</option><option>' ) + '</option>';
+			element = document.createElement('select');
+			element.setAttribute('selected', greeting);
+			element.innerHTML = '<option>' + greetings.join('</option><option>') + '</option>';
 			element.style.width = elemenWidth + 'px';
 			element.style.height = elemenHeight + 'px';
 			break;
 		case 'meter':
-			element = document.createElement( 'meter' );
-			element.setAttribute( 'min', '0' );
-			element.setAttribute( 'max', '100' );
-			element.setAttribute( 'value', Math.round( Math.random() * 100 ) );
+			element = document.createElement('meter');
+			element.setAttribute('min', '0');
+			element.setAttribute('max', '100');
+			element.setAttribute('value', Math.round(Math.random() * 100));
 			element.style.width = elemenWidth + 'px';
 			element.style.height = elemenHeight + 'px';
 			break;
 		case 'text':
 		default:
-			element = document.createElement( 'input' );
-			element.setAttribute( 'type', 'text' );
-			element.setAttribute( 'value', greeting );
+			element = document.createElement('input');
+			element.setAttribute('type', 'text');
+			element.setAttribute('value', greeting);
 			element.style.width = elemenWidth + 'px';
 			element.style.height = elemenHeight + 'px';
 	}
 
 	element.style.transform = `translate3d(${x}px, ${y}px, 0px) rotateX(${rx}deg) rotateY(${ry}deg) rotateZ(${rz}deg)`;
 
-	tree.appendChild( element );
+	tree.appendChild(element);
 }
 
-for( var i = 0; i < 300; i++ ) {
-	let element = document.createElement( 'input' );
-	element.setAttribute( 'type', 'radio' );
+for (var i = 0; i < 300; i++) {
+	let element = document.createElement('input');
+	element.setAttribute('type', 'radio');
 
-	let spread = window.innerWidth/2;
+	let spread = window.innerWidth / 2;
 
-	let x = Math.round( Math.random() * spread ) - ( spread / 4 ),
-		y = Math.round( Math.random() * height ),
-		z = Math.round( Math.random() * spread ) - ( spread / 2 );
+	let x = Math.round(Math.random() * spread) - (spread / 4),
+		y = Math.round(Math.random() * height),
+		z = Math.round(Math.random() * spread) - (spread / 2);
 
 	let rx = 0,
 		ry = Math.random() * 360,
 		rz = 0;
 
-	if( Math.random() > 0.5 ) element.setAttribute( 'checked', '' );
+	if (Math.random() > 0.5) element.setAttribute('checked', '');
 
 	element.style.transform = `translate3d(${x}px, ${y}px, ${z}px) rotateX(${rx}deg) rotateY(${ry}deg) rotateZ(${rz}deg)`;
 
-	tree.appendChild( element );
+	tree.appendChild(element);
 }
 
 function resize() {
-	tree.style.top = ( ( window.innerHeight - height - 100 ) / 2 ) + 'px';
+	tree.style.top = ((window.innerHeight - height - 100) / 2) + 'px';
 }
 
 resize();
